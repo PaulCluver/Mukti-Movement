e4a0e2f4e300ec8489492f822d3c8b3b36e06617
Paul Cluver <paulcl@sahomeloans.com>
2014-10-06 15:46:25 +0200

    Added js to contact form

--- app/templates/contact/contact.js
+++ app/templates/contact/contact.js
@@ -1,14 +1,57 @@
 'use strict';
 
 angular.module('muktiMovementApp.contact', ['ngRoute'])
+	.controller('contactCtrl', function($scope, $http, $log) {
+        
+        $scope.submit = function(form) {
+        	$scope.submitted = true;
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/contact', {
-    templateUrl: 'app/templates/contact/contact.html',
-    controller: 'contactCtrl'
+        	if (form.$invalid) {
+        		return;
+        	}
+
+        	var config = {
+        		params: {
+        			'callback' : 'JSON_CALLBACK',
+        			'firstName' : $scope.firstName,
+        			'lastName' : $scope.lastName,
+        			'email' : $scope.emailAddress,
+        			'questionOrComment' : $scope.questionOrComment
+        		},
+        	};
+
+        	var $promise = $http.jsonp('response.json', config)
+        		.success(function (data, status, headers, config) {
+        			if (data.status == 'OK') {
+        				$scope.firstName = null;
+        				$scope.lastName = null;
+        				$scope.emailAddress = null;
+        				$scope.questionOrComment = null;
+        				$scope.messages = "Your form has been sent";
+        				$scope.submitted = false;
+        			}
+        			else {
+        				$scope.messages = "Oops, we received your request, but there was an error processing it.";
+        				$log.error(data);
+        			}
+        		})
+        		.error (function (data, status, headers, config) {
+					$scope.progress = data;
+					$scope.messages = 'There was a network error. Try again later.';
+					$log.error(data);	
+        		})
+        		.finally (function() {
+        			$timeout(function() {
+        				$scope.messages = null;
+        			}, 3000) ;
+        		});
+
+        		//$scope.progress.addPromise($promise);
+        };
+	})	
+	.config(['$routeProvider', function($routeProvider) {
+  		$routeProvider.when('/contact', {
+    	templateUrl: 'app/templates/contact/contact.html',
+    	controller: 'contactCtrl'
   });
-}])
-
-.controller('contactCtrl', [function() {
-
 }]);
